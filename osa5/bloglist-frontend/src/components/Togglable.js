e9f7da9ad045608d71ff4c4f016cbbd3b1d6c99c
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-02-23 19:31:51 +0200

    5.11: blogilistan frontend, step11

--- osa5/bloglist-frontend/src/components/Togglable.js
+++ osa5/bloglist-frontend/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React, { useState, useImperativeHandle, forwardRef } from 'react'
+import PropTypes from 'prop-types'
 
 const Togglable = forwardRef((props, ref) => {
   const [visible, setVisibility] = useState(false)
@@ -26,5 +27,9 @@ const Togglable = forwardRef((props, ref) => {
     </div>
   )
 })
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable
