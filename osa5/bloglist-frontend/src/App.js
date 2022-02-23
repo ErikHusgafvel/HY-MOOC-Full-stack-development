f6a68cfdfcf6ceea6643e30fac60a972006c23d8
Erik Husgafvel <husgafvel.erik@gmail.com>
2022-02-23 14:00:13 +0200

    5.9*: blogilistan frontend, step9

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -143,7 +143,7 @@ const App = () => {
           </div>
           <br/>
           <div>
-            {blogs.map(blog =>
+            {blogs.sort((blog1, blog2) => (blog2.likes - blog1.likes)).map(blog =>
               <Blog key={blog.id} blog={blog} incrementBlogLikes={incrementBlogLikes}/>
             )}
           </div>
