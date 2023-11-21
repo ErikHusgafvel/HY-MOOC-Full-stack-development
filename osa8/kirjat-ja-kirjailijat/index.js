efeffbd5cd0782cc52bc0f59774edd8790bbd1ee
Erik Husgafvel <husgafvel.erik@gmail.com>
2023-11-21 23:33:27 +0700

    8.3: kaikki kirjailijat

--- osa8/kirjat-ja-kirjailijat/index.js
+++ osa8/kirjat-ja-kirjailijat/index.js
@@ -106,12 +106,14 @@ const typeDefs = `
     name: String!
     born: Int
     id: ID!
+    bookCount: Int!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -120,6 +122,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
   },
 }
 
