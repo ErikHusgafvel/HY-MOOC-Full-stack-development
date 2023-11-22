65c8e52b3dd1a605a12c2bbb1fb4274991d9da64
Erik Husgafvel <husgafvel.erik@gmail.com>
2023-11-22 22:29:52 +0700

    8.6: Kirjan lisäys

--- osa8/kirjat-ja-kirjailijat/index.js
+++ osa8/kirjat-ja-kirjailijat/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v1: uuid } = require("uuid")
 
 let authors = [
   {
@@ -115,6 +116,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -141,9 +151,25 @@ const resolvers = {
     },
     allAuthors: () => authors,
   },
+
   Author: {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+
+      const author = authors.find((author) => author.name === args.author)
+      if (!author) {
+        const newAuthor = { name: args.author, id: uuid() }
+        authors = authors.concat(newAuthor)
+      }
+
+      return book
+    },
   },
 }
 
